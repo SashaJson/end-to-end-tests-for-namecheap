a5729ed1a638c6d7d525745b52278413b169c6ce
SashaJson <sashamiller666@gmail.com>
2021-02-01 16:45:14 +0200

    added class authorizationPage

--- pages/authorizationPage.js
+++ pages/authorizationPage.js
@@ -1,11 +1,11 @@
 import BasePage from './basePage';
 
-export default class LoginPage extends BasePage {
+export default class AuthorizationPage extends BasePage {
 
     async isLoginFormDisplayed() {
         await page.waitForSelector('input[type="email"]');
         await page.waitForSelector('input[type="password"]');
-        await page.waitForSelector('button[type="submit"]');
+        await page.waitForSelector('button[class="btn block primary"]');
     }
 
     async login(
@@ -15,7 +15,18 @@ export default class LoginPage extends BasePage {
         await page.waitForSelector('input[type="email"]');
         await page.type('input[type="email"]', email);
         await page.type('input[type="password"]', password);
-        await page.click('button[type="submit"]');
     }
 
+    async submitLogin() {
+        await page.click('button[class="btn block primary"]');
+    }
+
+    async displayPassword() {
+        await page.click('button[class="btn-input btn-input-block"]');
+    }
+
+    async errorMessage() {
+        await page.waitForSelector('.ssls-notification__info');
+        return await page.$eval('.ssls-notification__info', element => element.innerText);
+    }
 }
