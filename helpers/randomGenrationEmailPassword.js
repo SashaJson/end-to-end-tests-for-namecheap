e09a767d667b808b68354c932173c818f8ee3438
SashaJson <sashamiller666@gmail.com>
2021-02-01 16:44:41 +0200

    added func for generation random data: email and password

---
+++ helpers/randomGenrationEmailPassword.js
@@ -0,0 +1,10 @@
+const {randexp} = require('randexp');
+
+module.exports = () => {
+
+    return {
+        email: randexp(/[a-z0-9]{4,15}@gmail\.com/),
+        password: randexp(/^[a-zA-Z0-9][0-9 a-z0-9]{4,9}/)
+    };
+
+}
