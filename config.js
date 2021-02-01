edea6f251f4925bf6ccba4d40b1883fb55051ebc
SashaJson <sashamiller666@gmail.com>
2021-02-01 16:46:35 +0200

    added to config.js jest time out

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    HOME_PAGE_URL: 'https://www.sbzend.ssls.com',
-
     EMAIL: 'ssls.automation+666@gmail.com',
-    PASSWORD: '123456'
+    PASSWORD: '123456',
+
+    JEST_TIME_OUT: 15000
 
 }
