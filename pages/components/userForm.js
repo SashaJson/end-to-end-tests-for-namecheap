3e589f051c40fa16b5dc1eea884d3096456cd852
SashaJson <sashamiller666@gmail.com>
2021-02-02 03:44:30 +0200

    added new methods to userForm class

--- pages/components/userForm.js
+++ pages/components/userForm.js
@@ -1,7 +1,27 @@
 export default class UserForm {
 
     async isUserFormDisplayed() {
-        await page.waitForSelector('button[class="ssls-btn waves-effect waves-classic ssls-btn--sm ssls-dropdown__toggle ssls-header-btn"]');
+        await page.waitForSelector('i[class="ssls-icon ssls-icon-user-circle"]');
+    }
+
+    async clickUserForm() {
+        await page.click('i[class="ssls-icon ssls-icon-user-circle"]');
+    }
+
+    async clickToProfile() {
+
+        // it is ugly, i'm know
+        await page.waitForSelector('ul[class="ssls-header-dropdown-nav ssls-header-user-nav"] > li:nth-child(2)');
+        await page.click('ul[class="ssls-header-dropdown-nav ssls-header-user-nav"] > li:nth-child(2)');
+
+    }
+
+    async clickLogOut() {
+
+        // it is ugly, i'm know
+        await page.waitForSelector('ul[class="ssls-header-dropdown-nav ssls-header-user-nav"] > li:nth-child(5)');
+        await page.click('ul[class="ssls-header-dropdown-nav ssls-header-user-nav"] > li:nth-child(5)');
+
     }
 
 }
